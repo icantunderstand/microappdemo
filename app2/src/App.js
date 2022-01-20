dcbcde34f0b773e650265dc625f5757216ed187e
sunhao <sunhao@sunhaodeMacBook-Pro.local>
2022-01-20 18:28:44 +0800

    feat: 初始化例子

---
+++ app2/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        this is a App2
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import history from 'history/browser';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { registerApplication, start } from 'single-spa'
+
+async function loadApp(libraryUrl, libraryName) {
+  // 打包的webpack配置是umd模式 直接挂载在window上
+  if(window[libraryName]) {
+    return window[libraryName]
+  }
+  // 加载并且等待js执行
+  await new Promise((resolve, reject) => {
+    const script = document.createElement('script')
+    script.src = libraryUrl
+    script.onload = resolve
+    script.onerror = reject
+    document.body.appendChild(script)
+  })
+  return window[libraryName]
+}
+
+// app组合可以通过走服务端下发配置模式 
+const apps = [
+  {
+    name: 'app1',
+    app: () => loadApp('http://localhost:3001/app1.js', 'app1'),
+    activeWhen: location => location.pathname.startsWith('/app1'),
+    customProps: {}
+  },
+  {
+    name: 'app2',
+    app: () => loadApp('http://localhost:3002/app2.js', 'app2'),
+    activeWhen: location => location.pathname.startsWith('/app2'),
+    customProps: {}
+  }
+]
+
+for(let i = 0; i < apps.length; i++) {
+  registerApplication(apps[i])
+}
+
+start()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    this is base app
+    <Router history={history}>
+      <div><Link to="app2"  >app2</Link></div>
+      <div><Link to="app1" >app1</Link></div>
+  </Router>
+  </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
